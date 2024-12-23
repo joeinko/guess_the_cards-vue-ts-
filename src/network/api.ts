9cf129d8da75514b73f59f4477f659b7c4a84454
Martin Koprna <joeblackofficial@gmail.com>
2024-12-23 02:50:17 +0100

    feat: added api call

---
+++ src/network/api.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const fetchRandomWord = async () => {
+    try {
+        const response = await axios.get<{word: string}>('https://api.api-ninjas.com/v1/randomword', {
+            headers: { 'X-Api-Key': import.meta.env.VITE_RANDOM_WORD_KEY }
+        });
+        return response.data.word;
+    } catch (error) {
+        console.error('Error fetching random word:', error);
+        return 'no word';
+    }
+}
