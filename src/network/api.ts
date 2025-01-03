1e0ca0a121564a2ae73de370dbc0de93969387bd
Martin Koprna <joeblackofficial@gmail.com>
2025-01-03 17:56:34 +0100

    fix: corrected error word in api

--- src/network/api.ts
+++ src/network/api.ts
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 export const fetchRandomWord = async () => {
     try {
-        const response = await axios.get<{word: string}>('https://api.api-ninjas.com/v1/randomword', {
+        const response = await axios.get<{word: string}>('https://api.api-ninjas.com/v3/randomword', {
             headers: { 'X-Api-Key': import.meta.env.VITE_RANDOM_WORD_KEY }
         });
-        return response.data.word;
+        return response.data.word[0];
     } catch (error) {
         console.error('Error fetching random word:', error);
-        return 'no word';
+        return 'FIXYOURAPI';
     }
 }
