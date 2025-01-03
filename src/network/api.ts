38d3edf117460d135911baed74f5c3043a8b21a6
Martin Koprna <joeblackofficial@gmail.com>
2025-01-03 18:00:40 +0100

    fix: corrected api

--- src/network/api.ts
+++ src/network/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchRandomWord = async () => {
     try {
-        const response = await axios.get<{word: string}>('https://api.api-ninjas.com/v3/randomword', {
+        const response = await axios.get<{word: string}>('https://api.api-ninjas.com/v1/randomword', {
             headers: { 'X-Api-Key': import.meta.env.VITE_RANDOM_WORD_KEY }
         });
         return response.data.word[0];
